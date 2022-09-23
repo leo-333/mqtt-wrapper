db88f4b6fd306867501d00906589b15591377de6
Jonas Leitner <jonas.leitner@eng-its.de>
2022-09-23 17:07:03 +0200

    added ws, added test client

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, WebSocket, status, Cookie, Query, Depends
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Cookie, Query, Depends
 from typing import Optional
 import requests
 import python_jwt as jwt
 import smtplib, ssl
+from email.mime.text import MIMEText
 
 app = FastAPI()
 
@@ -13,33 +14,45 @@ DEVICE_CODE_ENDPOINT = "/auth/realms/default/protocol/openid-connect/auth/device
 PUB_KEY_ENDPOINT = "/auth/realms/default"
 VERNEMQ_URL = "wss://mqtt.csp-staging.eng-softwarelabs.de"
 
-keycloakPubKey = None
+# SMTP Email Credentials
+SMTP_SENDER_EMAIL = "noreply@dlr.de"
+SMTP_PASSWORD = "1234"
+SMTP_PROXY = "smtp.gmail.com"
+SMTP_PORT = 465
+SMTP_RECEIVERS = ["jonas.leitner@eng-its.de"]
+
+keycloak_pub_key = None
+
 
 @app.on_event("startup")
 async def startup_event():
+    global keycloak_pub_key
     # Pull the PubKey from Keycloak
-    keycloakRes = requests.get(KEYCLOAK_URL + PUB_KEY_ENDPOINT)
-    keycloakPubKey = keycloakRes.json()['public_key']
+    keycloak_res = requests.get(KEYCLOAK_URL + PUB_KEY_ENDPOINT)
+    if keycloak_res.status_code == 200:
+        keycloak_pub_key = keycloak_res.json()['public_key']
+    else:
+        print('Keycloak seems to be down, exiting...')
+        exit(1)
 
 # HTTP Endpoint for getting an Device Code from Keycloak
 @app.get("/auth/device")
-async def getDeviceCode():
+async def get_device_code():
     # Proxy Request to Keycloak
-    keycloakRes = requests.post(KEYCLOAK_URL + DEVICE_CODE_ENDPOINT, {"client_id": CLIENT_ID}).json()
+    keycloak_res = requests.post(KEYCLOAK_URL + DEVICE_CODE_ENDPOINT, {"client_id": CLIENT_ID}).json()
 
     # Mail Auth URL to the User
-    if sendAuthMail(keycloakRes.verification_uri_complete):
-        # Return Keycloak Response to Device
-        return keycloakRes
-    else:
-        return {"error": "sending mail failed"}
+    send_auth_mail(keycloak_res.verification_uri_complete)
+    # Return Keycloak Response to Device
+    return keycloak_res
+
 
 async def get_cookie_or_token(
-    websocket: WebSocket,
-    session: Optional[str] = Cookie(None),
-    token: Optional[str] = Query(None),
+        websocket: WebSocket,
+        session: Optional[str] = Cookie(None),
+        token: Optional[str] = Query(None),
 ):
-    #if session is None and token is None:
+    # if session is None and token is None:
     #    await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
     return session or token
 
@@ -47,57 +60,74 @@ async def get_cookie_or_token(
 # WS Endpoint for MQTT Requests from the Device
 @app.websocket("/mqtt")
 async def websocket_endpoint(
-    websocket: WebSocket
+        websocket: WebSocket
 ):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        #print(websocket)
-        print(websocket.headers["Cookie"])
-        print(data)
-        continue
-        # Check Header for JWT
-        if not websocket.Headers:
-            await websocket.send_text("JWT Missing")
-            continue
+        try:
+            # Accept the initial MQTT Connection
+            #await websocket.send_text("CONNACK")
+            # Wait for the JWT
+            data = await websocket.receive()
+            print(data)
+            # Check Header for JWT
+            cookie = None
+            try:
+                cookie = websocket.headers["Cookie"]
+            except KeyError:
+                print("Cookie not found")
+                await websocket.send_text("JWT Missing")
+                continue
 
-        jwt = websocket.header.jwt
-
-        # Verify Authorization
-        if not verifyJWT(jwt): 
-            await websocket.send_text("JWT invalid")
-            continue
-        
-        # Proxy MQTT Request to VerneMQ
-        vernemqRes = await proxyRequestToVerneMQ(data)
-        
-        # Return VerneMQ Response
-        await websocket.send_text(vernemqRes)
+            # Verify Authorization
+            if not verify_jwt(cookie):
+                await websocket.send_text("JWT invalid")
+                continue
 
 
-def verifyJWT(token):
+            # JWT valid, lets accept the connection
+            await websocket.send_text("CONNACK")
+
+            # Proxy MQTT Request to VerneMQ
+            # vernemq_res = await proxy_request_to_vernemq(data)
+
+            # Return VerneMQ Response
+            await websocket.send_text(vernemq_res.json())
+        except WebSocketDisconnect:
+            print("Client has disconnected.")
+            break
+
+
+def verify_jwt(token):
     # Check if JWT is valid by verifying its signature with Keycloaks PubKey
-    header, claims = jwt.verify_jwt(token, keycloakPubKey, ['RS256'])
-    print(header)
-    print(claims)
-    
+    try:
+        header, claims = jwt.verify_jwt(token, keycloak_pub_key, ['RS256'])
+        print(header)
+        print(claims)
+    except:
+        return False
 
-async def sendAuthMail(AuthURL):
+    return True
+
+
+def send_auth_mail(auth_url):
     # Send an Authorization Email containing the Link returned by Keycloak to the User
+    msg = MIMEText(auth_url)
 
-
-    port = 465  # For SSL
-    password = input("Type your password and press enter: ")
+    msg['Subject'] = 'New MQTT Authorization Request'
+    msg['From'] = SMTP_SENDER_EMAIL
+    msg['To'] = SMTP_RECEIVERS[0]
 
     # Create a secure SSL context
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login("my@gmail.com", password)
-    # TODO: Send email here
+    with smtplib.SMTP_SSL(SMTP_PROXY, SMTP_PORT, context=context) as server:
+        server.login(SMTP_SENDER_EMAIL, SMTP_PASSWORD)
+        server.sendmail(SMTP_SENDER_EMAIL, SMTP_RECEIVERS, msg.as_string())
 
-    return True
+    print("Send new Auth URL: " + auth_url)
 
-async def proxyRequestToVerneMQ(data):
+
+async def proxy_request_to_vernemq(data):
     # TODO make it Websocket
-    return await requests.post(VERNEMQ_URL)
+    return requests.post(VERNEMQ_URL)
--- app/test.py
+++ app/test.py
@@ -1,11 +1,29 @@
 import requests
-from websocket import create_connection
+import paho.mqtt.client as mqtt
 
-ws = create_connection("ws://localhost:8000")
-print("Sending 'Hello, World'...")
-ws.send("Hello, World")
-print("Sent")
-print("Receiving...")
-result =  ws.recv()
-print("Received '%s'" % result)
-ws.close()
+jwt = 'nada'
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe("$SYS/#")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+client = mqtt.Client(transport="websockets")
+client.enable_logger()
+client.ws_set_options(path="/mqtt")
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set(username="", password=jwt)
+
+client.connect("localhost", 8000, 60)
+
+client.loop_forever()
